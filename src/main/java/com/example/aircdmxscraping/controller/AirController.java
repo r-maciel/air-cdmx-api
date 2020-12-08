253b114ea877c4b2d869f2c85d78c167232cd2b8
Roberto Maciel <lmaciel1303@gmail.com>
2020-12-08 09:32:30 -0600

    Done only for CDMX

--- src/main/java/com/example/aircdmxscraping/controller/AirController.java
+++ src/main/java/com/example/aircdmxscraping/controller/AirController.java
@@ -1,13 +1,8 @@
 package com.example.aircdmxscraping.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
-import javax.websocket.server.PathParam;
-
 import com.example.aircdmxscraping.api.AirCdmx;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
