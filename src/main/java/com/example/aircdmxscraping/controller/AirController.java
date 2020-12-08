3d03376902ecea025ce2a8f0df9ab72806cb17c9
Roberto Maciel <lmaciel1303@gmail.com>
2020-12-08 08:57:30 -0600

    Scraper CDMX

--- src/main/java/com/example/aircdmxscraping/controller/AirController.java
+++ src/main/java/com/example/aircdmxscraping/controller/AirController.java
@@ -2,6 +2,8 @@ package com.example.aircdmxscraping.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import javax.websocket.server.PathParam;
+
 import com.example.aircdmxscraping.api.AirCdmx;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AirController {
-    
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/cdmx")
 	public AirCdmx cdmx() {
